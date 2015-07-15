1e0cf338ade7575ad208dd61fac344530fc7fbbd
Rafael Benaion <rafael.benaion@hotmail.com>
2015-07-15 05:01:07 -0300

    teste f

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
+#nao esqueça
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
